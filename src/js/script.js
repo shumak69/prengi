1a96204751252c4957767c111290bd2e5373fd5b
Dmitriy <dima06444@gmail.com>
2022-06-20 11:55:42 +0300

    final

---
+++ src/js/script.js
@@ -0,0 +1,84 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.control__ul'),
+    countries = document.querySelector('.header__countries'),
+    link = document.querySelectorAll('.control__li');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    countries.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
+link.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.toggle('active');
+      countries.classList.remove('active');
+      hamburger.classList.toggle('active');
+    });
+});
+
+new WOW().init();
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+	speedAsDuration: true
+});
+
+const slides = document.querySelectorAll('.tablet__slider-content'),
+    next = document.querySelector('.tablet__slider-next'),
+    prev = document.querySelector('.tablet__slider-prev'),
+    tablet = document.querySelectorAll('.tablet__tab-items');
+let currentSlide = 0;
+
+function showContent() {
+    slides.forEach(item => {
+        item.classList.remove('active');
+    });
+    slides[currentSlide].classList.add('active');
+    tablet.forEach(item => {
+        item.classList.remove('active');
+    });
+    tablet[currentSlide].classList.add('active');
+}
+
+next.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(currentSlide == slides.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+    showContent();
+});
+prev.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(currentSlide == 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide--;
+    }
+    showContent();
+});
+
+document.querySelector('.tablet__tab').addEventListener('click', (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('tablet__tab-items')) {
+        tablet.forEach((item, i) => {
+            if(e.target == item) {
+                currentSlide = i;
+                showContent();
+            }
+        });
+    }
+});
+
+width = window.screen.width
+if(width <= 768) {
+    const descr = document.querySelector('.footer__descr');
+    
+    console.log();
+    if(descr.textContent.length > 250) {
+        descr.textContent = descr.textContent.substring(0, 250) + '...';
+    }
+}
+
+
